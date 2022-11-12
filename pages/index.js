bcb12d6576a677ef6dea194de9a5dd135e909541
Ncedo <anneldahnce@gmail.com>
2022-11-13 01:55:11 +0200

    Adding Home button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -9,9 +10,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="bg-white px-10">
         <section className="bg-blue-700 min-h-screen">
-          <h1>ANNELDAH NKUNA</h1>
+          <nav className="py-10 mb-12 flex justify-between">
+           <h1 className="text-xl" > Anneldah Portfolio</h1>
+           <ul className="flex items-center">
+            <li>
+              <BsFillMoonStarsFill/>
+            </li>
+            <li><a href="#">Home</a></li>
+           </ul>
+          </nav>
 
         </section>
       </main>
