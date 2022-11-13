6d5d039edae129683a9373b600087f79eb747b36
Ncedo <anneldahnce@gmail.com>
2022-11-13 09:55:48 +0200

    Added a style on the first heading of index file

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="bg-blue-700 min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-           <h1 className="text-xl" > Anneldah Portfolio</h1>
+           <h1 className="text-xl font-burtons" > Anneldah Portfolio</h1>
            <ul className="flex items-center">
             <li>
               <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
             </li>
             <li>
                 <a className=
-                "bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Home</a></li>
+                "bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py6-2 rounded-md ml-8" href="#">Home</a></li>
            </ul>
           </nav>
 
