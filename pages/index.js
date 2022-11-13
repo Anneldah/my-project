f1c0410274f200e526ea54667393c808f3740362
Ncedo <anneldahnce@gmail.com>
2022-11-13 02:03:01 +0200

    styling the home button

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,11 @@ export default function Home() {
            <h1 className="text-xl" > Anneldah Portfolio</h1>
            <ul className="flex items-center">
             <li>
-              <BsFillMoonStarsFill/>
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
             </li>
-            <li><a href="#">Home</a></li>
+            <li>
+                <a className=
+                "bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Home</a></li>
            </ul>
           </nav>
 
